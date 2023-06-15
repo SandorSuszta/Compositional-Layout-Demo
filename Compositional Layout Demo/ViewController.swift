735ea03d0f9b6ac946a1e50924b67dc83672e069
Sandor Suszta <sasha.shusta@gmail.com>
2023-06-15 13:40:04 +0100

    Initial Commit

---
+++ Compositional Layout Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Compositional Layout Demo
+//
+//  Created by Nataliia Shusta on 15/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
