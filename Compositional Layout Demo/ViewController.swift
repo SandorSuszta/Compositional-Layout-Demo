021eb1998a19968fd08f4d391cd63b50d189fd8e
Sandor Suszta <sasha.shusta@gmail.com>
2023-06-15 14:24:54 +0100

    Create Compositional Layout

--- Compositional Layout Demo/ViewController.swift
+++ Compositional Layout Demo/ViewController.swift
@@ -13,7 +13,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+       configureCollectionView()
+    }
+    
+    private func configureCollectionView() {}
+    
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        //1. Create size and item
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.25),
+            heightDimension: .fractionalWidth(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        //2. Create and configure group
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalWidth(0.25)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        //3. Create section
+        let section = NSCollectionLayoutSection(group: group)
+        
+        //4. Create layout
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
     }
 }
 
